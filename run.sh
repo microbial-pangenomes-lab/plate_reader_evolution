698cf38a4f60a143ef315c97fe89694d0281c3d3
Marco Galardini <galactus8403@gmail.com>
2022-04-21 11:38:09 +0200

    Run some jobs on 3 cores

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ mkdir -p out
 
 indir=$1
 
+touch myjobs.txt
+rm myjobs.txt || true
+
 for folder in $(find $indir -maxdepth 1 -type d -name 'EXP*Evol*');
 do
   echo $folder;exp=$(basename $folder | awk -F '_' '{print $1}');
@@ -12,9 +15,9 @@ do
   python3 parse_folder-runner.py $folder $indir/Plate\ Design\ Evol\ \(Vertical\).xlsx out/$exp/$exp"_evol.tsv";
   mkdir -p out/$exp/plate_plots;
   mkdir -p out/$exp/plate_plots/evol;
-  python3 plot_plate-runner.py out/$exp/$exp"_evol.tsv" out/$exp/plate_plots/evol;
+  echo "python3 plot_plate-runner.py out/$exp/$exp"_evol.tsv" out/$exp/plate_plots/evol" >> myjobs.txt;
   mkdir -p out/$exp/evol_plots/;
-  python3 plot_evol-runner.py out/$exp/$exp"_evol.tsv" out/$exp/evol_plots/;
+  echo "python3 plot_evol-runner.py out/$exp/$exp"_evol.tsv" out/$exp/evol_plots/" >> myjobs.txt;
 done
 
 for folder in $(find $indir -maxdepth 1 -type d -name 'EXP*MIC*');
@@ -24,7 +27,11 @@ do
   python3 parse_folder-runner.py --mic $folder $indir/Plate\ Design\ MIC\ \(Vertical\).xlsx out/$exp/$exp"_mic.tsv";
   mkdir -p out/$exp/plate_plots;
   mkdir -p out/$exp/plate_plots/mic;
-  python3 plot_plate-runner.py out/$exp/$exp"_mic.tsv" out/$exp/plate_plots/mic;
+  echo "python3 plot_plate-runner.py out/$exp/$exp"_mic.tsv" out/$exp/plate_plots/mic" >> myjobs.txt;
   mkdir -p out/$exp/mic_plots/;
-  python3 compute_mic-runner.py out/$exp/$exp"_mic.tsv" out/$exp/MICs.tsv --plot --plots-output out/$exp/mic_plots/;
+  echo "python3 compute_mic-runner.py out/$exp/$exp"_mic.tsv" out/$exp/MICs.tsv --plot --plots-output out/$exp/mic_plots/" >> myjobs.txt;
 done
+
+parallel --jobs 3 --progress < myjobs.txt
+
+rm myjobs.txt
