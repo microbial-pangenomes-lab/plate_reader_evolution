68e987bd7276927ab7763476a5da3a0a2ea4fc5a
Marco Galardini <marco.galardini@twincore.de>
2023-05-25 13:30:54 +0200

    Fix for MIC maker (9)

--- robot/mic_maker/protocol.ot2.py
+++ robot/mic_maker/protocol.ot2.py
@@ -254,8 +254,7 @@ def make_mic(protocol):
                     previous_column = column
                 # get rid of overhead drug + water in column 1
                 p300.aspirate(volume, plate.wells_by_name()[f'{row}22'])
-                p300.dispense(volume, p300.trash_container)
-                p300.blow_out()
+                p300.blow_out(p300.trash_container)
         # just drop it in the trash
         p300.drop_tip()
 
